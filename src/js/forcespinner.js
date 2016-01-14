d08bfb114e2bf34a9df0e172ef3137e72b4257e3
ravichandra1 <ravichandra.vn987@gmail.com>
2016-01-14 13:39:17 -0800

    Added the spinner color attribute to all the respective classes

--- src/js/forcespinner.js
+++ src/js/forcespinner.js
@@ -136,7 +136,11 @@ function forceSpin(elementId, spinnerType, spinnerColor, spinnerbgColor) {
       break;
     case "bounce":
       $('.force-child').css("background-color", spinnerColor);
-    
+     case "slds":
+      $('.slds_spinner div:after').css("background-color", spinnerColor);
+      $('.slds_spinner div:before').css("background-color", spinnerColor);
+      $('.slds_spinner:before').css("background-color", spinnerColor);
+      $('.slds_spinner:after').css("background-color", spinnerColor);
       break;
 
   }
